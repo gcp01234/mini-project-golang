cda6e32eed1c3180fba6d9fa5ee71200e07d1bb1
gitacitrap <puspita.gitacitra@gmail.com>
2022-08-13 22:03:14 +0700

    create struct data for tamu and response and create function koneksi

--- go/main.go
+++ go/main.go
@@ -1,16 +1,46 @@
+/*
+Mini project untuk pelatihan Golang PROA
+Author: Gita Citra Puspita
+Dibuat pada: 12 Agustus 2022
+*/
+
 package main
 
 import (
 	"fmt"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
+	"net/http"
+	"html/template"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main(){
-	_ , err := gorm.Open(sqlite.Open("data.db"), $gorm.Config{})
-	if err != nill {
-		panic ("Program error, tidak bisa terkoneksi dengan database")
-	}
-	fmt.Println("Koneksi database berhasil.")
+
+	fmt.Println("Cek.")
 
 }
+
+//struktur data tabel tamu
+type tamu struct{
+	Uuid string
+	NamaLengkap string
+	Domisili string
+	CreatedAt string
+	UpdatedAt string
+}
+
+//struktur data respon
+type response struct{
+	Status bool
+	Pesan string
+	Data []tamu
+}
+//fungsi untuk koneksi ke database mysql
+func koneksi() (*sql.DB, error){
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/undangan")
+	if err != nill{
+		return nill, err
+	}
+	return db, nill
+}
+
